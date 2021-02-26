b4d7d68f623dc535a5675c1370bdad73ea10496a
Lorenzo Kappeler <me@lnk.codes>
2021-02-26 15:13:09 +0100

    feat: acceppt the cookie banner to the ads are loading

--- cypress/integration/fetchAds.js
+++ cypress/integration/fetchAds.js
@@ -5,7 +5,7 @@ describe("Extract ads", () => {
     throw new Error("Please make sure to set CYPRESS_ADS_ENV")
   }
 
-  ["de", "fr", "it", "en"].forEach((language) => {
+  ;["de", "fr", "it", "en"].forEach((language) => {
     const fileName = `master-${Cypress.env("ADS_ENV")}-${language}-v3.json`
 
     it(`${language.toUpperCase()}: Extract and store ad images in json file`, () => {
@@ -14,34 +14,42 @@ describe("Extract ads", () => {
         onBeforeLoad: ($window) => {
           return new Cypress.Promise((resolve) => {
             const onAdLoaded = () => {
-              $window.document.removeEventListener("adHpEmotionalLoaded", onAdLoaded)
+              $window.document.removeEventListener(
+                "adHpEmotionalLoaded",
+                onAdLoaded
+              )
               resolve()
             }
-            $window.document.addEventListener("adHpEmotionalLoaded", onAdLoaded, true)
+            $window.document.addEventListener(
+              "adHpEmotionalLoaded",
+              onAdLoaded,
+              true
+            )
           })
-        }})
-        .then(() => {
-          cy.get("#tatm-adHpEmotional")
-          cy.get("#tatm-adHpEmotional[data-ad]", { timeout: 20000 }).then(
-            ($ad) => {
-              const ad =
-                $ad[0].dataset && $ad[0].dataset.ad
-                  ? JSON.parse($ad[0].dataset.ad)
-                  : {}
-              cy.writeFile(`${targetPath}${fileName}`, ad)
+        },
+      }).then(() => {
+        cy.get('button[id="onetrust-accept-btn-handler"]').click()
+        cy.get("#tatm-adHpEmotional")
+        cy.get("#tatm-adHpEmotional[data-ad]", { timeout: 20000 }).then(
+          ($ad) => {
+            const ad =
+              $ad[0].dataset && $ad[0].dataset.ad
+                ? JSON.parse($ad[0].dataset.ad)
+                : {}
+            cy.writeFile(`${targetPath}${fileName}`, ad)
 
-              // Legacy suppport
-              if (language === "de") {
-                const fileNameV0 = "master.json"
-                cy.writeFile(`${targetPath}${fileNameV0}`, {})
-                const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
-                cy.writeFile(`${targetPath}${fileNameV1}`, {})
-                const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
-                cy.writeFile(`${targetPath}${fileNameV2}`, ad)
-              }
+            // Legacy suppport
+            if (language === "de") {
+              const fileNameV0 = "master.json"
+              cy.writeFile(`${targetPath}${fileNameV0}`, {})
+              const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
+              cy.writeFile(`${targetPath}${fileNameV1}`, {})
+              const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
+              cy.writeFile(`${targetPath}${fileNameV2}`, ad)
             }
-          )
-        })
+          }
+        )
+      })
     })
   })
 })
