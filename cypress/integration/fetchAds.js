c6f7fce54900a132e2118515aca80bad577c47dc
Lorenzo Kappeler <me@lnk.codes>
2021-05-04 20:47:08 +0200

    chore: add more logs and more explicit selector

--- cypress/integration/fetchAds.js
+++ cypress/integration/fetchAds.js
@@ -30,36 +30,56 @@ describe("Extract ads", () => {
       }).then(() => {
         cy.get('button[id="onetrust-accept-btn-handler"]').click()
         cy.get("#tatm-adHpEmotional")
-        cy.get("#tatm-adHpEmotional[data-ad]", { timeout: 20000 }).then(
-          ($ad) => {
+        cy.get("div .ad-loaded > #tatm-adHpEmotional[data-ad]", {
+          timeout: 20000,
+        }).then(($ad) => {
+          const ad =
+            $ad[0].dataset && $ad[0].dataset.ad
+              ? JSON.parse($ad[0].dataset.ad)
+              : {}
+          cy.log("condition")
+          cy.log(!!($ad[0].dataset && $ad[0].dataset.ad))
+          cy.log("has node")
+          cy.log(!!$ad[0])
+          cy.log("has dataset")
+          cy.log(!!$ad[0].dataset)
+          cy.log("has dataset ad")
+          cy.log(!!$ad[0].dataset.ad)
+          cy.log("dataset")
+          cy.log($ad[0].dataset.ad)
+          cy.writeFile(`${targetPath}${fileName}`, ad)
+
+          cy.get("div .ad-loaded > #tatm-adHpEmotional[data-ad]", {
+            timeout: 20000,
+          }).then(($ad) => {
             const ad =
               $ad[0].dataset && $ad[0].dataset.ad
                 ? JSON.parse($ad[0].dataset.ad)
                 : {}
-            debugger
-            cy.log("condition")
+
+            cy.log("INSIDE condition")
             cy.log(!!($ad[0].dataset && $ad[0].dataset.ad))
-            cy.log("has node")
+            cy.log("INSIDE has node")
             cy.log(!!$ad[0])
-            cy.log("has dataset")
+            cy.log("INSIDE has dataset")
             cy.log(!!$ad[0].dataset)
-            cy.log("has dataset ad")
+            cy.log("INSIDE has dataset ad")
             cy.log(!!$ad[0].dataset.ad)
-            cy.log("dataset")
+            cy.log("INSIDE dataset")
             cy.log($ad[0].dataset.ad)
             cy.writeFile(`${targetPath}${fileName}`, ad)
+          })
 
-            // Legacy suppport
-            if (language === "de") {
-              const fileNameV0 = "master.json"
-              cy.writeFile(`${targetPath}${fileNameV0}`, {})
-              const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
-              cy.writeFile(`${targetPath}${fileNameV1}`, {})
-              const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
-              cy.writeFile(`${targetPath}${fileNameV2}`, ad)
-            }
+          // Legacy suppport
+          if (language === "de") {
+            const fileNameV0 = "master.json"
+            cy.writeFile(`${targetPath}${fileNameV0}`, {})
+            const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
+            cy.writeFile(`${targetPath}${fileNameV1}`, {})
+            const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
+            cy.writeFile(`${targetPath}${fileNameV2}`, ad)
           }
-        )
+        })
       })
     })
   })
