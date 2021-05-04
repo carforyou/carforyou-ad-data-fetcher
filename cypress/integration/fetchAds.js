27f35962592e3d1b07d8463b2f4f4357237fbf38
Lorenzo Kappeler <me@lnk.codes>
2021-05-04 22:17:20 +0200

    chore: readd usefull parts

--- cypress/integration/fetchAds.js
+++ cypress/integration/fetchAds.js
@@ -29,27 +29,26 @@ describe("Extract ads", () => {
         },
       }).then(() => {
         cy.get('button[id="onetrust-accept-btn-handler"]').click()
-        cy.reload(true)
         cy.get("#tatm-adHpEmotional")
-        cy.get("#tatm-adHpEmotional[data-ad]", { timeout: 20000 }).then(
-          ($ad) => {
-            const ad =
-              $ad[0].dataset && $ad[0].dataset.ad
-                ? JSON.parse($ad[0].dataset.ad)
-                : {}
-            cy.writeFile(`${targetPath}${fileName}`, ad)
+        cy.get("div .ad-loaded > #tatm-adHpEmotional[data-ad]", {
+          timeout: 20000,
+        }).then(($ad) => {
+          const ad =
+            $ad[0].dataset && $ad[0].dataset.ad
+              ? JSON.parse($ad[0].dataset.ad)
+              : {}
+          cy.writeFile(`${targetPath}${fileName}`, ad)
 
-            // Legacy suppport
-            if (language === "de") {
-              const fileNameV0 = "master.json"
-              cy.writeFile(`${targetPath}${fileNameV0}`, {})
-              const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
-              cy.writeFile(`${targetPath}${fileNameV1}`, {})
-              const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
-              cy.writeFile(`${targetPath}${fileNameV2}`, ad)
-            }
+          // Legacy suppport
+          if (language === "de") {
+            const fileNameV0 = "master.json"
+            cy.writeFile(`${targetPath}${fileNameV0}`, {})
+            const fileNameV1 = `master-${Cypress.env("ADS_ENV")}.json`
+            cy.writeFile(`${targetPath}${fileNameV1}`, {})
+            const fileNameV2 = `master-${Cypress.env("ADS_ENV")}-v2.json`
+            cy.writeFile(`${targetPath}${fileNameV2}`, ad)
           }
-        )
+        })
       })
     })
   })
